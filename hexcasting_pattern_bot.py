f8f9fc524977d778323c646da970325af0c62148
Katte <tacocatytreal@gmail.com>
2022-06-12 19:00:30 -0700

    Fixed branch merge issues

--- hexcasting_pattern_bot.py
+++ hexcasting_pattern_bot.py
@@ -27,10 +27,10 @@ async def on_ready():
     description='Sends a gif of the pattern you enter, as found in the book.'
 )
 async def pattern(ctx, pattern : str):
-    if pattern in client.pattern_index:
+    if pattern.casefold().replace("'", "").replace(":", "") in (pat.casefold().replace("'", "").replace(":", "") for pat in client.pattern_index):
         for i in range(len(client.pattern_index)):
-            if pattern == client.pattern_index[i]:
-                assemble = disnake.Embed(color = disnake.Colour.purple(), title=client.pattern_index[i], url=client.web_link[i])
+            if pattern.casefold().replace("'", "").replace(":", "") == client.pattern_index[i].casefold().replace("'", "").replace(":", ""):
+                assemble = disnake.Embed(color = disnake.Colour.purple(), title=client.pattern_index[i], url=f"https://gamma-delta.github.io/HexMod/#{client.web_link[i]}")
                 assemble.set_image(url=client.gif_list[i])
                 await ctx.send(embed=assemble)
     else:
@@ -44,16 +44,15 @@ async def pattern(ctx, pattern : str):
     description='Sends a link to the online guidebook, courtesy of Alwinfy.'
 )
 async def guide(ctx):
-    assemble = disnake.Embed(color = disnake.Colour.purple(), title='Online Hexcasting Guide', url='https://bl.ocks.org/Alwinfy/raw/455d2d4af12bd893b46170056ca1714f/')
+    assemble = disnake.Embed(color = disnake.Colour.purple(), title='Online Hexcasting Guide', url='https://gamma-delta.github.io/HexMod/')
     await ctx.send(embed=assemble)
 
 @client.slash_command(
-    name='constants',
-    description='Sends a link to the section on numerical constants in the online book.'
+    name='topic',
+    description='Sends a link to the online guidebook section for the given topic.'
 )
-async def guide(ctx):
-    assemble = disnake.Embed(color = disnake.Colour.purple(), title='Numerical Constants', url='https://bl.ocks.org/Alwinfy/raw/455d2d4af12bd893b46170056ca1714f/#patterns/consts')
+async def guide(ctx, topic : str):
+    assemble = disnake.Embed(color = disnake.Colour.purple(), title=f'Online Hexcasting Guide: {topic}', url=f'https://gamma-delta.github.io/HexMod/#patterns/{topic.lower().replace(" ", "_")}')
     await ctx.send(embed=assemble)
 
-
-client.run('OTcxODMwNDIxNDU1MjY5OTE4.YnQNnA.o4lVGboEc-YBrARj8UDmjH0jzg4')
+client.run()
